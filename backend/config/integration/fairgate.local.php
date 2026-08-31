822827302b2ddfe5044d16ac2403ec1e2a26b029
Charley Collins <charley@collins.ch>
2026-08-31 22:00:19 +0200

    Prepare Integrationstests

---
+++ backend/config/integration/fairgate.local.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'mode' => 'fake',
+    'base_url' => '',
+    'organization_id' => '',
+    'access_key' => '',
+    'public_key' => '',
+];
---
+++ backend/config/integration/mail.local.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'mailer_dsn' => 'null://null',
+    'from_address' => 'integration@example.invalid',
+    'from_name' => 'Integration Tests',
+];
